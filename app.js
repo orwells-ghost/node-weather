358d697a9a1d33966ba989062b56cbd8edb10409
Ryan Kennedy <ryan+github@ryankennedy.me>
2019-11-13 18:42:24 -0800

    Final 1.0.0

--- app.js
+++ app.js
@@ -1,32 +1,20 @@
-const request = require('request')
+const forecast = require('./utils/forecast')
+const geocode = require('./utils/geocode')
 
-const weatherUrl = 'https://api.darksky.net/forecast/788e9a14778e9badf24e30a4697ce191/37.8267,-122.4233'
+const address = process.argv[2] 
 
-// Since we set json to true, json will be parsed for us automatically
-request({ url: weatherUrl, json: true }, (error, response) => {
+// Using return in the error if statement will end the execution of this function
+geocode(address || 'Portland', (error, data) => {
     if (error) {
-        console.log('Unable to connect to weather service')
+        return console.log(error)
     }
-    else if (response.body.error) {
-        console.log('Unable to find location')
-    }
-    else {
-        console.log('The current temperature is ' + response.body.currently.temperature + ". There is a " + Math.round(response.body.currently.precipProbability * 100) + "% chance of rain.")
-    }
-})
 
-const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiMTIzcnlhbnBrIiwiYSI6ImNrMndwZzNnMzBoeWEzZHBnZ3E2NnAxN2MifQ.BvsO4hLTI_FEq-GN_uSg4w&limit=1'
-
-request({ url: geoUrl, json: true }, (error, response) => {
-    if (error) {
-        console.log('Unable to connect to weather service')
-    }
-    else if (response.body.features.length === 0) {
-        console.log('Unable to find location')
-    }
-    else {
-        const latitude = response.body.features[0].geometry.coordinates[1]
-        const longitude = response.body.features[0].geometry.coordinates[0]
-        console.log(`Latitude is ${latitude} and longitude is ${longitude}`)
-    }
+    forecast(data.latitude, data.longitude, (error, forecastData) => {
+        if (error) {
+            return console.log(error)
+        }
+        
+        console.log(data.location)
+        console.log(forecastData)
+    })
 })
